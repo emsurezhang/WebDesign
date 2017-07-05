865886665fb6e73030ccc4e546b4cb83377e8c16
emsurezhang <emsurezhang@gmail.com>
2017-07-05 19:27:24 +0800

    ImageSlider20170705

--- ImageSlider/carousel.js
+++ ImageSlider/carousel.js
@@ -15,6 +15,15 @@
 
   var Carousel = function (element, options) {
     this.$element    = $(element)
+    //.carousel-inidicators 是Image页面的角标，页面上显示的小圆点。HTML的代码如下
+    /**
+     * <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+     */
     this.$indicators = this.$element.find('.carousel-indicators')
     this.options     = options
     this.paused      = null
@@ -22,7 +31,11 @@
     this.interval    = null
     this.$active     = null
     this.$items      = null
-
+    //Returns expr1 if it can be converted to false; otherwise, returns expr2. Thus, when used with Boolean values, && returns true if both operands are true; otherwise, returns false.
+    //逻辑运算符 && 计算法过程，如果expr1为false则返回expr1；否则返回expr2。
+    //expr1 判定为false的条件是:null undefined ±0 NaN false the empty string
+    //这里的实际意义是代替if(判断条件) {执行语句} 为 判断条件 && 执行语句
+    //当判断条件为true时，不能返回expr1，&&计算符检查expr2，从而执行&&后面的操作。不论expr2返回的是true或false，都不重要，因为目的是执行expr2
     this.options.keyboard && this.$element.on('keydown.bs.carousel', $.proxy(this.keydown, this))
 
     this.options.pause == 'hover' && !('ontouchstart' in document.documentElement) && this.$element
@@ -60,8 +73,25 @@
     this.options.interval
       && !this.paused
       && (this.interval = setInterval($.proxy(this.next, this), this.options.interval))
-
+    this.progress();
     return this
+  }
+  
+  Carousel.prototype.progress = function () {
+    this.options._progress_interval = 10;
+    this.options._step = 100/this.options.interval*this.options._progress_interval;
+    setInterval($.proxy(this.progress_step,this),this.options._progress_interval);
+  }
+  Carousel.prototype.progress_step = function (){
+    var cur = parseInt(this.$indicators.find('.active').find('.progress-bar').css("width"));
+    console.log(cur);
+    if(cur < 100) {
+      cur = cur+this.options._step;
+      if(cur<1)
+    } else {
+      cur = 100;
+    }
+    this.$indicators.find('.active').find('.progress-bar').css("width",cur+"%");
   }
 
   Carousel.prototype.getItemIndex = function (item) {
@@ -116,6 +146,13 @@
 
   Carousel.prototype.slide = function (type, next) {
     var $active   = this.$element.find('.item.active')
+    //Returns expr1 if it can be converted to true; otherwise, returns expr2. Thus, when used with Boolean values, || returns true if either operand is true.
+    // || 运算符代表 表达式1如果为true，则返回表达式1；否则返回表达式2；
+    // 所以我们经常写的if( expr1 || expr2 )，这个操作 ||并不直接产生true、false的结果。而是根据expr1、expr2返回对应的表达式expr3。
+    // 再由if(expr3)产生结果。
+    // 对一个表达式expr1是否为true，javascript有如下判断规则:
+    // 变量如果不为0，null，undefined，false，都会被处理为true;反之为false。
+    // 所以 next作为一个函数传递参数，如果为空，则$next 被 this.getItemForDirection(type, $active)所替换。
     var $next     = next || this.getItemForDirection(type, $active)
     var isCycling = this.interval
     var direction = type == 'next' ? 'left' : 'right'
